4e4d973c107d48789a0d693838d33da759309c15
FabyGiacomini <fabygiacomini@ymail.com>
2023-03-12 12:38:30 -0300

    Refactoring - microservices in different modules

---
+++ common-kafka/src/main/java/br/com/wasp/ecommerce/ConsumerFunction.java
@@ -0,0 +1,7 @@
+package br.com.wasp.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public interface ConsumerFunction<T> {
+    void consume(ConsumerRecord<String, T> record);
+}
--- src/main/java/br/com/wasp/ConsumerFunction.java
+++
@@ -1,7 +0,0 @@
-package br.com.wasp;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public interface ConsumerFunction<T> {
-    void consume(ConsumerRecord<String, T> record);
-}
