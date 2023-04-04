1ece55e7f31911a45575ba5db81f96f43d6b7ba5
FabyGiacomini <fabygiacomini@ymail.com>
2023-04-04 10:08:43 -0300

    Fix NewOrderMain

--- service-new-order/src/main/java/br/com/wasp/ecommerce/NewOrderMain.java
+++ service-new-order/src/main/java/br/com/wasp/ecommerce/NewOrderMain.java
@@ -14,12 +14,13 @@ public class NewOrderMain {
                     var orderId = UUID.randomUUID().toString();
                     var amount = new BigDecimal(Math.random() * 5000 + 1);
                     var email = Math.random() + "email.com";
+                    var correlationId = new CorrelationId(NewOrderMain.class.getSimpleName());
 
                     var order = new Order(orderId, amount, email);
-                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, order);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, correlationId, order);
 
                     var emailCode = "Thank you for your order! We are processing your order!";
-                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailCode);
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, correlationId, emailCode);
                 }
             }
         }
